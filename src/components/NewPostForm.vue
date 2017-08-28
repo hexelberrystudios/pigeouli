686c3b5213c1f41ee5e258d3e7e881fc57bb6986
Jonathan S. Collins León <yotsubato32@gmail.com>
2017-08-28 07:32:34 -0500

    Adding new post validation

--- src/components/NewPostForm.vue
+++ src/components/NewPostForm.vue
@@ -4,13 +4,19 @@
       <img src="/static/img/profile.png" alt="An image of a pigeon" class="pg-np-pigeon" />
       <span class="pg-np-username">{{ username }}</span>
     </div>
-    <textarea rows="10" cols="50"></textarea>
+    <label for="new-post" class="pg-sr-only">Post Content</label>
+    <textarea id="new-post"
+              rows="10"
+              cols="50"
+              @input="update"></textarea>
+    <span @class="{ 'pg-chars-over': remainingCharCount < 0 }">{{ remainingCharCount }}</span>
     <submit-button text="Post"></submit-button>
     <span>{{ charactersRemaining }}</span>
   </form>
 </template>
 
 <script>
+import { mapState } from 'vuex';
 import SubmitButton from './SubmitButton';
 
 export default {
@@ -19,9 +25,45 @@ export default {
     this.$store.dispatch('form/resetForm');
   },
   computed: {
+    // get the latest value of this form field from the vuex store
+    ...mapState({
+      post(state) {
+        return state.form.fields['post'];
+      }
+    }),
+    remainingCharCount() {
+      const maxCharCount = 140;
 
+      return maxCharCount - this.post.length;
+    }
   },
   methods: {
+    update(e) {
+      let i,
+        currentChar;
+      let post = e.target.value;
+      let allowableCharacters = ['c', 'o', '!', '?', '.'];
+
+      // go through the post and make sure only the allowable characters are in the string
+      post = Array.prototype.map.call(post, function (char) {
+        // automatically replace any offending characters with either a 'c' or 'o'
+        const randomCharacter = Math.floor(Math.random() * (max - min + 1));
+
+        if (allowableCharacters.indexOf(char) > -1) {
+          // accepted character
+          return char;
+        } else {
+          // replacing offending character
+          return allowableCharacters[randomCharacter];
+        }
+      }).join('');
+
+      // tell action in form vuex module to update its form field with the following key/value pair
+      this.$store.dispatch('form/updateField', {
+        name: 'post',
+        value: post
+      })
+    },
     post(e) {
       const self = this;
       // let username = self.$store.state.form.fields.username;
@@ -68,5 +110,9 @@ export default {
   .pg-button {
     float: right;
     margin: 0.5rem;
+  }
+
+  .pg-chars-over {
+    color: red;
   }
 </style>
