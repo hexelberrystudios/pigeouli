c6c4d0f324a52a9f52368c3ed6a4e945263c80a1
Jonatron Leon <yotsubato32@gmail.com>
2017-09-04 10:41:47 -0500

    Client development in progress

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,49 +1,51 @@
 <template>
   <form v-on:submit="login" name="login" method="post" action="/login">
-    <text-field id="username"
-                label="username"
-                type="text"
-                placeholder="username"></text-field>
-    <text-field id="passphrase"
-                label="passphrase"
-                type="passphrase"
-                placeholder="passphrase"></text-field>
+    <text-field id="username" label="username" type="text" placeholder="username"></text-field>
+    <text-field id="passphrase" label="passphrase" type="passphrase" placeholder="passphrase"></text-field>
     <submit-button text="Login"></submit-button>
   </form>
 </template>
 
 <script>
-  import TextField from './TextField';
-  import SubmitButton from './SubmitButton';
-  
-  export default {
-    name: 'login-form',
-    created() {
-      this.$store.dispatch('form/resetForm');
-    },
-    methods: {
-      login(e) {
-        const self = this;
-        e.preventDefault();
+import TextField from './TextField';
+import SubmitButton from './SubmitButton';
 
-        // @TODO: ajax call to login, where the server will return the user's db url
-        // for us to sync with
+export default {
+  name: 'login-form',
+  created() {
+    this.$store.dispatch('form/resetForm');
+  },
+  methods: {
+    login(e) {
+      const fields = this.$store.state.form.fields;
+      e.preventDefault();
+
+      this.$http.post('/login', {
+        email: fields.email,
+        passphrase: fields.passphrase
+      }).then(response => {
+        // success
+        console.log(response);
         self.$router.push('/dashboard');
-      }
-    },
-    components: {
-      TextField,
-      SubmitButton
+      }, response => {
+        // error
+        console.error(response);
+      });
     }
-  };
+  },
+  components: {
+    TextField,
+    SubmitButton
+  }
+};
 </script>
 
 <style scoped>
-  form {
-    display: grid;
-  }
+form {
+  display: grid;
+}
 
-  form > * {
-    justify-self: center;
-  }
+form>* {
+  justify-self: center;
+}
 </style>
--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -4,78 +4,82 @@
       <label id="username-label" class="pg-dark-text-shadow">username</label>
       <span id="username" class="pg-dark-text-shadow">{{ username }}</span>
     </div>
-    <text-field id="email"
-                label="email"
-                type="email"></text-field>
-    <text-field id="passphrase"
-                label="passphrase"
-                type="password"></text-field>
+    <text-field id="email" label="email" type="email"></text-field>
+    <text-field id="passphrase" label="passphrase" type="password"></text-field>
     <p class="pg-dark-text-shadow">(The passphrase is ideally a few random words, but can be anything as long as it's at least three characters.)</p>
     <submit-button text="I'm in"></submit-button>
   </form>
 </template>
 
 <script>
-  import TextField from './TextField';
-  import SubmitButton from './SubmitButton';
-  
-  export default {
-    name: 'register-form',
-    computed: {
-      username: {
-        get() {
-          let result = '';
+import TextField from './TextField';
+import SubmitButton from './SubmitButton';
 
-          if (this.$store.state.form.fields && this.$store.state.form.fields.username) {
-            result = this.$store.state.form.fields.username;
-          } else {
-            this.$router.push('/name');
-          }
+export default {
+  name: 'register-form',
+  computed: {
+    username: {
+      get() {
+        let result = '';
 
-          return result;
+        if (this.$store.state.form.fields && this.$store.state.form.fields.username) {
+          result = this.$store.state.form.fields.username;
+        } else {
+          this.$router.push('/name');
         }
-      }
-    },
-    methods: {
-      register(e) {
-        const self = this;
-        // let username = self.$store.state.form.fields.username;
-        e.preventDefault();
 
-        // @TODO: ajax call to register, where CouchDB url will be created,
-        // and the url to sync with will be returned.
-        // Prerequisite: Authorization token for signing up for premium.
-        self.$router.push('/dashboard');
+        return result;
       }
-    },
-    components: {
-      TextField,
-      SubmitButton
     }
-  };
+  },
+  methods: {
+    register(e) {
+      const fields = this.$store.state.form.fields;
+      // let username = self.$store.state.form.fields.username;
+      e.preventDefault();
+
+      this.$http.post('/register', {
+        username: fields.username,
+        email: fields.email,
+        passphrase: fields.passphrase
+      }).then(response => {
+        // success
+        console.log(response);
+        self.$router.push('/dashboard');
+      }, response => {
+        // error
+        console.error(response);
+      });
+    }
+  },
+  components: {
+    TextField,
+    SubmitButton
+  }
+};
 </script>
 
 <style scoped>
-  form {
-    display: grid;
-  }
+form {
+  display: grid;
+}
 
-  form > * {
-    justify-self: center;
-  }
+form>* {
+  justify-self: center;
+}
 
-  #username-container {
-    text-align: center;
-  }
+#username-container {
+  text-align: center;
+}
 
-  #username-label {
-    display: block;
-    font-size: 1.25em;
-  }
+#username-label {
+  display: block;
+  font-size: 1.25em;
+}
 
-  #username {
-    display: block;
-    font-size: 1.25em;
-    margin: 0.5em 0;
-  }
+#username {
+  display: block;
+  font-size: 1.25em;
+  margin: 0.5em 0;
+}
 </style>
