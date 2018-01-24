8e770d035884dc09fca2ec8fe701372a71cf251f
Jonatron Leon <yotsubato32@gmail.com>
2018-01-24 09:14:32 -0600

    Updated server port

--- config/sql-config.js
+++ config/sql-config.js
@@ -1,4 +1,13 @@
+var localConfig = {
+  host     : process.env.SQL_HOST,
+  user     : process.env.SQL_USER,
+  password : process.env.SQL_PASSWORD,
+  database : process.env.SQL_DATABASE,
+  timezone : 'UTC'
+};
+var config = process.env.DB_URL || localConfig;
+
 module.exports = {
   client: 'mysql',
-  connection: process.env.DB_URL
+  connection: config
 };
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ var app = express();
 var bodyParser = require('body-parser');
 var setupSession = require('./app/setup-session');
 var router = require('./app/routes');
+var PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
@@ -15,6 +16,6 @@ setupSession(app);
 // routes
 app.use('/', router);
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('listening...');
 });
